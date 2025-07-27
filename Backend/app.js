6e61a4393f52ed37d8e477e53f7e778e584fdc9e
alok bisnaliya <alokbisnaliya2@gmail.com>
2025-07-27 21:38:56 +0530

    Redeploy to Render

--- Backend/app.js
+++ Backend/app.js
@@ -50,3 +50,6 @@ app.use('/api/graph', require('./AdminRoutes/salesGraphRouter'));
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
+
+console.log("Redeployed at", new Date());
